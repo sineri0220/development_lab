83d6ead5db31a69e605a80a71e17d32c53d4ebfb
Jiwoo Shin <i0am0istj@gmail.com>
2024-04-30 16:27:46 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 
-import Home from './newappfirst';
+import Home from './newappfirst.js';
 import Calculator from './firstapp';
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <nav>
-        <Link to='/home'>Home</Link>
+        <Link to='/'>Home</Link>
         <Link to ='/calculator'>Calculator</Link>
       </nav>
       <Routes>
@@ -18,8 +18,38 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/calculator' element={<Calculator />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+// import './App.css';
+
+// import Home from './newappfirst';
+// import Calculator from './firstapp';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <nav>
+//         <Link to='/home'>Home</Link>
+//         <Link to ='/calculator'>Calculator</Link>
+//       </nav>
+//       <Routes>
+//       	{/* '/'로 접근할 경우 Home 컴포넌트로 렌더링 */}
+//         <Route path='/' exact element={<Home />} />
+//         <Route path='/home' element={<Home />} />
+//         <Route path='/calculator' element={<Calculator />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
